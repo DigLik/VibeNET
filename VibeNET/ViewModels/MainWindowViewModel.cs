8a4c70c69ee56e2267c69ecf67686bfeb3b8a2a1
DigLik <125634228653a@gmail.com>
2025-03-12 23:58:57 +0500

    Добавьте файлы проекта.

---
+++ VibeNET/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace VibeNET.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        [ObservableProperty]
+        private int _counter;
+
+        [RelayCommand]
+        private void Increment()
+        {
+            Counter++;
+        }
+    }
+}
