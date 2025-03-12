6c710f94fdb7539279dab6213352b113dd03f4b6
DigLik <125634228653a@gmail.com>
2025-03-13 01:27:46 +0500

    Упрощение кода и улучшение интерфейса приложения
    Удалена зависимость от `Avalonia.Diagnostics` в `VibeNET.csproj`.
    Изменён метод `Build` в `ViewLocator` для упрощения логики.
    Добавлены новые зависимости в `MainWindowViewModel`, удалено поле `_counter`, добавлено поле `_folderPath` и `_folderItems`. Реализован метод `PickFolder` для выбора папки.
    Обновлена разметка в `MainWindow.axaml`, заменён `StackPanel` на `Grid`, добавлены определения колонок и строк, изменены привязки для отображения элементов папки.

--- VibeNET/ViewLocator.cs
+++ VibeNET/ViewLocator.cs
@@ -6,7 +6,6 @@ using VibeNET.ViewModels;
 namespace VibeNET;
 public class ViewLocator : IDataTemplate
 {
-
     public Control? Build(object? param)
     {
         if (param is null)
@@ -15,12 +14,9 @@ public class ViewLocator : IDataTemplate
         var name = param.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
         var type = Type.GetType(name);
 
-        if (type != null)
-        {
-            return (Control)Activator.CreateInstance(type)!;
-        }
-
-        return new TextBlock { Text = "Not Found: " + name };
+        return type != null
+            ? (Control)Activator.CreateInstance(type)!
+            : new TextBlock { Text = "Not Found: " + name };
     }
 
     public bool Match(object? data)
